6173b8ca1941c5bc4ca3f1387821c8dd9db5af8c
Jay Shepherd <jdshep89@hotmail.com>
2016-01-07 21:30:48 -0700

    Unit tests for IP Function - Already pointing to some problems

--- ipfromwebpage_tests.py
+++ ipfromwebpage_tests.py
@@ -18,3 +18,25 @@ class TestValidate_url(TestCase):
 
     def test_malformed_url(self):
         self.assertFalse(validate_url('http://example'))
+
+
+class TestIp_from_string(TestCase):
+    empty = set()
+
+    def test_valid_single(self):
+        self.assertEquals(ip_from_string('192.168.0.1'), {'192.168.0.1'})
+
+    def test_valid_multiple(self):
+        self.assertEquals(ip_from_string('192.168.0.1 192.168.5.5'), {'192.168.0.1', '192.168.5.5'})
+
+    def test_duplicates(self):
+        self.assertEquals(ip_from_string('192.168.0.4 10.2.3.4 192.168.0.4 10.2.3.4'), {'192.168.0.4', '10.2.3.4'})
+
+    def test_word(self):
+        self.assertEquals(ip_from_string('word'), self.empty)
+
+    def test_invalid(self):
+        self.assertEquals(ip_from_string('999.999.999.999'), self.empty)
+
+    def test_close(self):
+        self.assertEquals(ip_from_string('1234.12341.12.3.3.4'), self.empty)
