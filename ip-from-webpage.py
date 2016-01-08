a33df93c0bee1b5670eb5a3a570b0a99c89f0a82
Jay Shepherd <jdshep89@hotmail.com>
2016-01-07 20:33:02 -0700

    split up code to allow for unit testing

--- ip-from-webpage.py
+++ ip-from-webpage.py
@@ -2,25 +2,14 @@
 
 __author__ = 'Jay Shepherd'
 
-from urllib.request import urlopen
-from urllib.parse import urlparse
 import re
+from urllib.parse import urlparse
+from urllib.request import urlopen
 
 import bs4
 
 
-def ip_from_webpage(url_input):
-    """Scrapes text of a URL page for IP addresses and returns as set.
-    :param url_input: Fully-qualified URL to scrape for IP Addresses
-    :return: set
-    """
-    ip_address = re.compile('(?:[0-9]{1,3}\.){3}[0-9]{1,3}(?:\/[0-9]{1,2})?')
-    data = bs4.BeautifulSoup(urlopen(url_input), 'html.parser')
-    data = str(data.get_text)
-    return set(ip_address.findall(data))
-
-
-def input_url():
+def interactive_url_input():
     """
     :return: Printed result of URL scrape
     """
@@ -28,23 +17,49 @@ def input_url():
     while not validate_url(url_input):
         url_input = input("Please enter a url to scrape for IPs. The URL must include http:// or https://: ")
         print('')
-    address_list = ip_from_webpage(url_input)
-    if address_list:
-        print("\n".join(address_list))
-    else:
-        print("No ips found when scraping {}".format(url_input))
+    return url_input
 
 
 def validate_url(url_arg):
-    """ Takes string input and validates if it is fully qualified url for passing into urlopen
+    """ Takes string input and validates if it is fully qualified url for passing into other functions
     :param url_arg: URL to check for validation
     :return: Boolean result of validation check
     """
     return urlparse(url_arg).scheme is not ''
 
 
-if __name__ == '__main__':
+def get_webpage_text(url_input):
+    """
+    Extracts text of webpage and returns
+    :param url_input: Fully-qualified URL to scrape for IP Addresses
+    :return: Text of webpage
+    """
+    data = bs4.BeautifulSoup(urlopen(url_input), 'html.parser')
+    return str(data.get_text)
+
+
+def ip_from_string(string):
+    """
+    Takes a string and extracts all IP Addresses as a SET
+    :param string: Any string of data
+    :return: IP Addresses as a SET
+    """
+    ip_address = re.compile('(?:[0-9]{1,3}\.){3}[0-9]{1,3}(?:\/[0-9]{1,2})?')
+    return set(ip_address.findall(string))
+
+
+def main():
     try:
-        input_url()
+        url_input = interactive_url_input()
+        webpage_text = get_webpage_text(url_input)
+        address_list = ip_from_string(webpage_text)
+        if address_list:
+            print("\n".join(address_list))
+        else:
+            print("No ips found when scraping {}".format(url_input))
     except KeyboardInterrupt:
         print("\nGoodbye")
+
+
+if __name__ == '__main__':
+    main()
